fbf74d1380a3d22afc273570e211a0df32d57192
nolanglenn <nolangglenn@gmail.com>
2019-03-30 17:30:16 -0500

    Javascript added. Just need handlebars.

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,29 @@
 // Import MySQL connection.
 const connection = require("../config/connection.js");
+
+const orm = {
+    // This method will select everything from the burgers table
+    selectAll: function (tableInput, cb) {
+        let queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableInput], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function (tableInput, colToSearch, valOfCol, cb) {
+        let queryString = "INSERT INTO ?? WHERE ?? = ?";
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function (tableInput, colToSearch, valOfCol, cb) {
+        let queryString = "UPDATE ?? SET ?? WHERE ?? = ?";
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
