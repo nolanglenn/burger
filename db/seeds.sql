f8845c62ae5d8a9c3c7a6e2a4da48c8c02390a98
nolanglenn <nolangglenn@gmail.com>
2019-03-30 13:06:02 -0500

    Setup files and connections

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheese", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Royal with Cheese", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chicken Burg", true);
